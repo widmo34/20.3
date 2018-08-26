42d4801dadf3f953f95be01e1a71ce09e03ed0a3
Radoslaw Weidemann <walecznyopos@gmail.com>
2018-08-26 19:07:45 +0200

    add some files

---
+++ react-router-sample/src/containers/flag-container.component.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import CountryFlagList from  '../presentational/flag-list.component';
+import { getCountries } from '../actions/actions-countries';
+
+class CountryFlagContainer extends Component{
+    constructor(props){
+        super(props);
+    }
+
+  
+
+    render(){
+        return(
+            <div>
+                <CountryFlagList countries={this.props.countries} />
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = function (store){
+        // console.log(store);
+    return{
+          countries: store.countriesReducer.countries
+    };
+};
+
+export default connect(mapStateToProps)(CountryFlagContainer);
